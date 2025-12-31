5cc9778ab0ca869eb240ea0a07250751b5ca74bf
Aryan Soni <web.vault0516@gmail.com>
2025-12-31 20:44:59 +0530

    revised sitemap for robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
   siteUrl: 'https://isoftcube-technologies.onrender.com/', // Change to your actual domain
-  generateRobotsTxt: true, // Generate robots.txt file
   sitemapSize: 7000, // Optional: split sitemaps if you have many URLs
   changefreq: 'weekly', // Default change frequency
   priority: 0.7, // Default priority
